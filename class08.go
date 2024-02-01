ad0377901787f10167936de6f48ebf68283429d1
Tiago Costa <tiagodpv@gmail.com>
2024-01-31 20:59:20 -0500

    last

--- class08.go
+++ class08.go
@@ -10,7 +10,11 @@ func main() {
 		//destination()
 		//movieSwitch()
 		//foodSwitch()
-		musicGenres()
+		//musicGenres()
+		//twoVariablesSwitch()
+		forLoopExample()
+		forLoopExample2()
+		forLopopMatrix()
 }
 
 
@@ -170,4 +174,105 @@ func destination() {
 
 	}
 
+ }
+
+ func animalFoodSwitch() {
+
+	animal := "monkey"
+	food := "bananas"
+ 
+	switch { //no need to mention variable here as we have more than one variable
+ 
+	case animal == "rabbit" && (food == "carrots" || food == "lettuce"):
+	 fmt.Println("The rabbit gains super speed")
+	case animal == "cat" && food == "fish":
+	 fmt.Println("The cat is now invisible")
+	case animal == "dog" && food == "bones":
+	 fmt.Println("The dog has acquired the power of super strength")
+	case animal == "bird" && food == "seeds":
+	 fmt.Println("The bird can now control the weather")
+	case animal == "monkey" && food == "bananas":
+	 fmt.Println("The money gains telekinetic abilities")
+	default:
+	 fmt.Println("This combination of animal and food doesn't yield any special powers")
+ 
+	}
+ }
+
+ func twoVariablesSwitch() {
+
+	var sport, season string
+ 
+	fmt.Println("Enter a sport")
+	fmt.Scanln(&sport)
+ 
+	fmt.Println("Enter a season")
+	fmt.Scanln(&season)
+ 
+	switch {
+	case season == "winter" && (sport == "skiing" || sport == "hockey" || sport == "snowboarding"):
+	 fmt.Println("Winter is perfect for skiing, hockey and snowboarding")
+	case season == "summer" && sport == "soccer":
+	 fmt.Println("Summer is perfect for soccer")
+	case season == "spring" && sport == "baseball":
+	 fmt.Println("Spring is perfect for baseball")
+	case season == "autumn" && sport == "football":
+	 fmt.Println("Autumn is perfect for football")
+	case season == "summer" && sport == "swimming":
+	 fmt.Println("Swimming is refreshing in the summer")
+	default:
+	 fmt.Println("This combination of sport and season is not typical")
+ 
+	}
+ 
+ }
+
+ func forLoopExample() {
+
+	//Calculate the sum of numbers from 1 to 10
+ 
+	sum := 0
+ 
+	for i := 1; i <= 10; i++ {
+ 
+	 sum += i
+ 
+	}
+ 
+	fmt.Println(sum)
+ 
+}
+
+
+func forLoopExample2() {
+
+ fruits := [3]string{"apples", "banana", "orange"}
+
+ for i := 0; i < len(fruits); i++ {
+
+  fmt.Println(fruits[i])
+
+ }
+
+}
+
+func forLopopMatrix() {
+
+	matrix := [3][3]int{
+ 
+	 {1, 2, 3},
+	 {4, 5, 6},
+	 {7, 8, 9},
+	}
+ 
+	for i := 0; i < len(matrix); i++ {
+ 
+	 for j := 0; j < len(matrix); j++ {
+ 
+		fmt.Print(matrix[i][j], " ")
+ 
+	 }
+	 fmt.Println()
+ 
+	}
  }
